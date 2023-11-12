c0cf6a5bf337f706bba0484407f5b9978f12a097
parker <129785640+parkerstafford@users.noreply.github.com>
2023-11-11 23:58:16 -0700

    Init cpu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 
-int main() {
 
-    
+struct CPU {
+    using Byte = unsigned char;
+    using Word = unsigned short;
+
+    Word PC; // Program Counter
+    Word SP;    // Stack Pointer
+
+    Byte A, X, Y; // Registers
+
+    Byte C : 1; // Status Flags
+    Byte Z : 1;
+    Byte I : 1;
+    Byte D : 1;
+    Byte B : 1;
+    Byte V : 1;
+    Byte N : 1;
+
+};
+
+int main() {
+    CPU cpu;
+
     return 0;
 }
